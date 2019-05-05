b3a6026be90dc5e6d57d6628b253b7d9f1089bc8
ahsan-habib-m4 <ahsan.habib@metafour.com>
2019-05-05 23:22:01 +0600

    College list page design

--- js/custom.js
+++ js/custom.js
@@ -16,11 +16,16 @@ $( document ).ready(function() {
 	  var check = $('#filter-modal').find('input[type=checkbox]:checked').length;
 	  $(".filter-count").html(check);
 	});
+
   	$('.clear-all-btn').click(function() {
 	  $('#filter-modal').find('input[type=checkbox]:checked').prop("checked", false);
 	  $(".filter-count").html("0");
 	});
-
-
+  	$("a.nav-link").click(function() {
+  		var elm = $(this).attr("id");
+  		$('a.nav-link').removeClass("active"); 		
+  		$("#"+elm).children(".searchRadio").prop("checked", true);
+  		$("#"+elm).addClass("active");
+  	});
 });
   
