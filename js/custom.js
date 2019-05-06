74dacbbbcce0f6523d9d885ebd91f6af84f275a2
ahsan-habib-m4 <ahsan.habib@metafour.com>
2019-05-06 19:29:54 +0600

    College list to search page radio button selection

--- js/custom.js
+++ js/custom.js
@@ -27,5 +27,36 @@ $( document ).ready(function() {
   		$("#"+elm).children(".searchRadio").prop("checked", true);
   		$("#"+elm).addClass("active");
   	});
+
+  	function getParameterByName(name, url) {
+	    if (!url) url = window.location.href;
+	    name = name.replace(/[\[\]]/g, '\\$&');
+	    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+	        results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+	}
+
+	var srcType = getParameterByName('type'); 
+
+	if(srcType == "all"){
+		$('a.nav-link').removeClass("active"); 	
+		$("#srcByall").prop("checked", true);
+		$("#srcByall").parent("a").addClass('active');
+	} else if (srcType == "reach") {
+		$('a.nav-link').removeClass("active"); 	
+		$("#srcByreach").prop("checked", true);
+		$("#srcByreach").parent("a").addClass('active');
+	} else if (srcType == "match") {
+		$('a.nav-link').removeClass("active"); 	
+		$("#srcBymatch").prop("checked", true);
+		$("#srcBymatch").parent("a").addClass('active');
+	} else if (srcType == "safety") {
+		$('a.nav-link').removeClass("active"); 	
+		$("#srcBySaftety").prop("checked", true);
+		$("#srcBySaftety").parent("a").addClass('active');
+	}
+
 });
   
